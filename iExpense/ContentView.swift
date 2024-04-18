7560333d9e2f57286c784ec68c77841513894d8a
tesddev <tesleem.amuda@gmail.com>
2024-04-18 14:10:22 +0100

    use @observable class as source of truth between 2 pages

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -9,21 +9,56 @@ import SwiftUI
 import Observation
 
 @Observable
-class User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+class Expenses {
+    var items = [ExpenseItem](){
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+
+        items = []
+    }
 }
 
 struct ContentView: View {
-    @State private var user = User()
+    
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
 
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(expenses.items) { item in
+                        Text(item.name)
+                    }
+                    .onDelete(perform: removeItems)
+                }
+                .navigationTitle("iExpense")
+            }
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    showingAddExpense = true
+                }
+            }
         }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: expenses)
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
@@ -32,4 +67,10 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-  
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
